9821a7b6fde3ffce08a369492b81f83923c4ba1a
umar-alv1 <umaralvi816@gmail.com>
2022-09-04 22:50:09 -0700

    array

---
+++ app.js
@@ -0,0 +1,63 @@
+// // Problem
+// var fullName0:string = "ali"
+// var fullName1:string = "umar"
+// var fullName2:string = "naveed"
+// var fullName3:string = "fahad"
+// var fullName4:string = "fawad"
+// var fullName5:string = "faisal"
+// // Array
+// var fullNames = ["ali","umar", "naveed", "fahad", "fawad", "faisal"]
+// console.log("fullNames",fullNames);
+// Array Index
+// fullNames[0]
+// console.log("fullNames[0]",fullNames [0]);
+// fullNames[1]
+// console.log("fullNames[1]",fullNames [1]);
+// fullNames[2]
+// console.log("fullNames[2]",fullNames [2]);
+// fullNames[3]
+// console.log("fullNames[3]",fullNames [3]);
+// fullNames[4]
+// console.log("fullNames[4]",fullNames [4]);
+// fullNames[5]
+// console.log("fullNames[5]",fullNames [5]);
+// for (let index = 0; index < 4; index++) {
+//     console.log("array print by index", fullNames[3]);    
+// }
+// Check Length of Array
+// console.log("length of array", fullNames.length);
+// // var numbers:number[] = [10,20,30,40,50]
+// var userNames:string[] = new Array(5)
+// userNames = ["ali", "naveed", "asad"]
+// userNames[0] = "ali"
+// userNames[1] = "ali"
+// userNames[2] = "ali"
+// userNames[3] = "ali"
+// userNames[4] = "ali"
+// userNames[5] = "ali"
+// console.log("userNames", userNames.length);
+// push
+// var studentMarks:number[] = [150,100,200]
+// console.log("studenMarks",studentMarks);
+// studentMarks.push(130)
+// console.log("studenMarks",studentMarks);
+// Pop
+var studentMarks = [150, 100, 200, 400];
+// console.log("studenMarks",studentMarks);
+// studentMarks.push(130)
+// console.log("studenMarks",studentMarks);
+// var studentMarks:number[] = [150,100,200]
+// console.log("studenMarks",studentMarks);
+// studentMarks.pop()
+// studentMarks.pop()
+// console.log("studenMarks",studentMarks);
+// studentMarks.unshift(148)
+// console.log("studentMarks after unshift", studentMarks);
+// studentMarks.shift()
+// console.log("studentMarks after shift", studentMarks);
+// studentMarks.splice(0,1,)
+// studentMarks.splice(2,1,350)
+// studentMarks.splice(0,0,350)
+// studentMarks.splice(4,0,450)
+studentMarks.splice(4, 0, 350);
+console.log("studentMarks after splice", studentMarks);
